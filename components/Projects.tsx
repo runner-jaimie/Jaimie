c159e7264743a263e5ca3878cd3bc1bba479fc69
runner-jaimie <jlee723@myseneca.ca>
2025-11-17 13:03:10 -0500

    feat: add initial Projects section structure

---
+++ components/Projects.tsx
@@ -0,0 +1,18 @@
+export default function Projects() {
+  return (
+    <section id="projects" className="w-full px-12 py-24">
+      <div className="max-w-[1280px] mx-auto">
+        {/* Title */}
+        <h2 className="text-[64px] font-bold tracking-wide leading-none text-gray-200">
+          PROJECTS
+        </h2>
+
+        {/* 아래 선 */}
+        <hr className="border-t border-[#3A3A3A] mt-4 mb-20" />
+
+        {/* 여기 안에 카드들이 들어갈 거야 */}
+        <div>{/* TODO: 프로젝트 카드들 */}</div>
+      </div>
+    </section>
+  );
+}
